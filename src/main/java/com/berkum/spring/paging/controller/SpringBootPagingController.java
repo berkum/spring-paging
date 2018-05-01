ac8f41a142385bc767992b76a982b628dca388b2
Alain Bergeron <alainc.bergeron@bnc.ca>
2018-05-01 16:10:37 -0400

    configurable spring paging param names

--- src/main/java/com/berkum/spring/paging/controller/SpringBootPagingController.java
+++ src/main/java/com/berkum/spring/paging/controller/SpringBootPagingController.java
@@ -1,7 +1,9 @@
 package com.berkum.spring.paging.controller;
 
+import com.berkum.spring.paging.business.api.PersonBusiness;
+import com.berkum.spring.paging.domain.Person;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -10,36 +12,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.berkum.spring.paging.business.api.PersonBusiness;
-import com.berkum.spring.paging.domain.Person;
-import com.github.lemniscate.struct.paging.PagingModule;
-
 @RestController
 public class SpringBootPagingController {
 
-	@Autowired
-	private PersonBusiness business;
+  @Autowired
+  private PersonBusiness business;
 
-	@Bean
-	public PagingModule pagingModule() {
-		return new PagingModule();
-	}
+  // @Bean
+  // public PagingModule pagingModule() {
+  // return new PagingModule();
+  // }
 
-	@RequestMapping(value = "/listPageable", method = RequestMethod.GET)
-	Page<Person> employeesPageable(Pageable pageable) {
-		Page<Person> persons = business.findAll(pageable);
-		return persons;
+  @RequestMapping(value = "/listPageable", method = RequestMethod.GET)
+  Page<Person> employeesPageable(Pageable pageable) {
+    Page<Person> persons = business.findAll(pageable);
+    return persons;
 
-	}
+  }
 
-	@RequestMapping(value = "/listPageableRE", method = RequestMethod.GET)
-	ResponseEntity<Page<Person>> employeesPageableRE(Pageable pageable) {
-		return ResponseEntity.ok(business.findAll(pageable));
-	}
+  @RequestMapping(value = "/listPageableRE", method = RequestMethod.GET)
+  ResponseEntity<Page<Person>> employeesPageableRE(Pageable pageable) {
+    return ResponseEntity.ok(business.findAll(pageable));
+  }
 
-	@RequestMapping(value = "/listPageableLN", method = RequestMethod.GET)
-	ResponseEntity<Page<Person>> employeesPageable2(Pageable pageable, @RequestParam("lastName") String lastName) {
-		return ResponseEntity.ok(business.findByLastName(pageable, lastName));
-	}
+  @RequestMapping(value = "/listPageableLN", method = RequestMethod.GET)
+  ResponseEntity<Page<Person>> employeesPageable2(Pageable pageable, @RequestParam("lastName") String lastName) {
+    return ResponseEntity.ok(business.findByLastName(pageable, lastName));
+  }
 
 }
